f124665aa8d95c9f66aa3b644463e3d9ac84bd1c
Shawn Wolter <woltersh@outlook.de>
2023-04-22 01:04:03 +0200

    first commit

---
+++ db/index.js
@@ -0,0 +1,41 @@
+// ℹ️ package responsible to make the connection with mongodb
+// https://www.npmjs.com/package/mongoose
+const mongoose = require("mongoose");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+// ℹ️ Sets the MongoDB URI for our app to have access to it.
+// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
+const MONGO_URI = process.env.MONGODB_URI
+
+mongoose
+  .connect(MONGO_URI)
+  .then((x) => {
+    const databaseName = x.connections[0].name;
+    console.log(`Connected to Mongo! Database name: "${databaseName}"`);
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
+
+  // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(MONGO_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
---
+++ routes/index.routes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const db = require("mongodb")
+
+/* GET home page */
+router.get("/", (req, res, next) => {
+  res.render("index");
+});
+
+/*test route to check if we receive data from mongodb atlas*/
+router.get("/", (req, res, next) => {
+  db.find()
+    .then(data => {
+      console.log(data)
+      res.render("index", { data: data })
+    })
+})
+
+module.exports = router;
