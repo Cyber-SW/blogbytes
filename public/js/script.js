a0053e5c090bda96f1b381ef7e47853a6b289a05
hussain <muhammadhussain170117@gmail.com>
2023-04-28 09:20:29 +0200

    almost finished

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,20 @@
+var swiper = new Swiper(".mySwiper", {
+	effect: "coverflow",
+	grabCursor: true,
+	centeredSlides: true,
+	slidesPerView: "auto",
+	coverflowEffect: {
+	  rotate: 0,
+	  stretch: 0,
+	  depth: 300,
+	  modifier: 1,
+	  slideShadows: false,
+	},
+	pagination: {
+	  el: ".swiper-pagination",
+	},
+  });
+
 const inputs = document.querySelectorAll(".input");
 
 
